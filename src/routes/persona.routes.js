54e33193f2e235fc1360d1eb9f92ddbbb99c5465
Samuel Bustamante <samubustamante@hotmail.com>
2022-11-15 12:09:49 -0300

    Controladores para editar usuario y persona

--- src/routes/persona.routes.js
+++ src/routes/persona.routes.js
@@ -3,14 +3,16 @@ const { verificarToken } = require('../middlewares/verificacion-jwt');
 const { verificarRol }   = require('../middlewares/verificar-rol');
 const { 
     crearNuevaPersona,
-    obtenerPersonaPorNroDocumento
+    obtenerPersonaPorNroDocumento,
+    editarPersona
 } = require('../controllers/persona.controller');
  
 const router             = Router();
 
 //Crear nueva persona
-router.post('/person', verificarToken,crearNuevaPersona);
+router.post('/person', verificarToken, verificarRol,crearNuevaPersona);
 //Obtener persona por nro de documento
 router.get('/person/:nro', verificarToken,obtenerPersonaPorNroDocumento);
-
+//Editar persona
+router.put('/person/:idPersona', verificarToken,verificarRol, editarPersona);
 module.exports = router;
