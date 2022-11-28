4f6142786e529669ddebcbe6cea937b08f676bfc
Samuel Bustamante <54921441+samubus19@users.noreply.github.com>
2022-11-28 12:00:46 -0300

    Correccion en la comprobacion de que el documento no este repetido al editar

--- src/controllers/documento.controller.js
+++ src/controllers/documento.controller.js
@@ -7,26 +7,26 @@ const {
 } = require("./schemas/documentoSchema");
 
 async function crearNuevoDocumento(req, res) {
-  console.log(new Date().toLocaleDateString("es-ES", { timeZone: "GMT" }));
   const bodyData = {
-    nroDocumento: req.body.nroDocumento,
-    tipoDocumento: req.body.tipoDocumento,
-    descripcion: req.body.descripcion,
-    fechaIngreso: new Date().toLocaleDateString("es-ES", { timeZone: "GMT" }),
+    nroDocumento      : req.body.nroDocumento,
+    tipoDocumento     : req.body.tipoDocumento,
+    descripcion       : req.body.descripcion,
+    fechaIngreso      : new Date().toLocaleDateString("es-ES", { timeZone: "GMT" }),
     // fechaIngreso      : "17/11/2022",
-    fechaSalida: "",
-    estado: "Iniciado",
-    sede: "MESAENTRADA",
-    idUsuarioFirmante: req.body.idUsuarioFirmante,
+    fechaSalida       : "",
+    estado            : "Iniciado",
+    sede              : "MESAENTRADA",
+    idUsuarioFirmante : req.body.idUsuarioFirmante,
   };
 
-  console.log(bodyData.fechaIngreso);
   try {
     Joi.assert(bodyData, DocumentoSchema);
 
     const documento = await Documento.findOne({
-      nroDocumento: bodyData.nroDocumento,
+      nroDocumento  : bodyData.nroDocumento,
+      tipoDocumento : bodyData.tipoDocumento
     });
+
     if (documento) {
       return res.status(400).json({
         statusCode: 400,
@@ -35,16 +35,16 @@ async function crearNuevoDocumento(req, res) {
     }
 
     const nuevoDocumento = new Documento({
-      nroDocumento: bodyData.nroDocumento,
-      tipoDocumento: bodyData.tipoDocumento,
-      descripcion: bodyData.descripcion,
-      historial: [
+      nroDocumento  : bodyData.nroDocumento,
+      tipoDocumento : bodyData.tipoDocumento,
+      descripcion   : bodyData.descripcion,
+      historial     : [
         {
-          fechaIngreso: bodyData.fechaIngreso,
-          fechaSalida: bodyData.fechaSalida,
-          estado: bodyData.estado,
-          sede: bodyData.sede,
-          idUsuarioFirmante: bodyData.idUsuarioFirmante,
+          fechaIngreso      : bodyData.fechaIngreso,
+          fechaSalida       : bodyData.fechaSalida,
+          estado            : bodyData.estado,
+          sede              : bodyData.sede,
+          idUsuarioFirmante : bodyData.idUsuarioFirmante,
         },
       ],
     });
@@ -53,6 +53,7 @@ async function crearNuevoDocumento(req, res) {
     return res.status(201).json({
       mensaje: "Documento creado correctamente",
     });
+
   } catch (error) {
     console.log(error);
     return res.status(500).json({
@@ -64,25 +65,37 @@ async function crearNuevoDocumento(req, res) {
 async function editarDocumento(req, res) {
   const _id = req.body._id;
   const bodyData = {
-    tipoDocumento: req.body.tipoDocumento,
-    nroDocumento: req.body.nroDocumento,
-    descripcion: req.body.descripcion,
+    tipoDocumento : req.body.tipoDocumento,
+    nroDocumento  : req.body.nroDocumento,
+    descripcion   : req.body.descripcion,
     //Agregar usuario que editó
   };
 
   try {
     Joi.assert(bodyData, editarDocumentoSchema);
     const documento = await Documento.findOne({ _id: _id });
+
     if (documento) {
-      documento.nroDocumento = bodyData.nroDocumento;
+      const documentoPorTipo  = await Documento.findOne({nroDocumento : bodyData.nroDocumento, tipoDocumento : bodyData.tipoDocumento})
+      
+      if(documentoPorTipo) {
+        return res.status(400).json({
+          mensaje : "Error en la petición. Ya existe un documento con ese número y tipo"
+        })
+      }
+
+      documento.nroDocumento  = bodyData.nroDocumento;
       documento.tipoDocumento = bodyData.tipoDocumento;
-      documento.descripcion = bodyData.descripcion;
+      documento.descripcion   = bodyData.descripcion;
     }
+
     await documento.save();
     return res.status(200).json({
       mensaje: "Documento editado correctamente",
     });
+
   } catch (error) {
+    console.log(error)
     return res.status(500).json({
       mensaje: error,
     });
@@ -91,37 +104,32 @@ async function editarDocumento(req, res) {
 
 async function actualizarEstadoDocumento(req, res) {
   const _id = req.body._id;
-  console.log(_id);
   const bodyData = {
-    nuevoEstado: req.body.nuevoEstado,
-    sede: req.body.sede,
-    fechaIngreso: new Date().toLocaleDateString("es-ES", { timeZone: "GMT" }),
-    fechaSalida: "",
-    idUsuarioFirmante: req.body.idUsuarioFirmante,
+    nuevoEstado       : req.body.nuevoEstado,
+    sede              : req.body.sede,
+    fechaIngreso      : new Date().toLocaleDateString("es-ES", { timeZone: "GMT" }),
+    fechaSalida       : "",
+    idUsuarioFirmante : req.body.idUsuarioFirmante,
   };
+
   try {
     Joi.assert(bodyData, actualizarEstadoDocumentoSchema);
 
     const documento = await Documento.findOne({ _id: _id });
     if (documento) {
       documento.historial.push({
-        fechaIngreso: bodyData.fechaIngreso,
-        fechaSalida: bodyData.fechaSalida,
-        estado: bodyData.nuevoEstado,
-        sede: bodyData.sede,
-        idUsuarioFirmante: bodyData.idUsuarioFirmante,
+        fechaIngreso      : bodyData.fechaIngreso,
+        fechaSalida       : bodyData.fechaSalida,
+        estado            : bodyData.nuevoEstado,
+        sede              : bodyData.sede,
+        idUsuarioFirmante : bodyData.idUsuarioFirmante,
       });
 
-      documento.historial[documento.historial.length - 2].fechaSalida =
-        documento.historial[documento.historial.length - 1].fechaIngreso;
+      documento.historial[documento.historial.length - 2].fechaSalida = documento.historial[documento.historial.length - 1].fechaIngreso;
       // documento.historial[documento.historial.length-1].sede        = documento.historial[documento.historial.length-2].destino
-      if (
-        documento.historial[documento.historial.length - 1].estado ===
-        "Finalizado"
-      ) {
+      if (documento.historial[documento.historial.length - 1].estado === "Finalizado") {
         documento.historial[documento.historial.length - 1].sede = "";
-        documento.historial[documento.historial.length - 1].fechaSalida =
-          new Date().toLocaleDateString("es-ES", { timeZone: "GMT" });
+        documento.historial[documento.historial.length - 1].fechaSalida = new Date().toLocaleDateString("es-ES", { timeZone: "GMT" });
       }
     }
 
@@ -130,8 +138,8 @@ async function actualizarEstadoDocumento(req, res) {
     return res.status(200).json({
       mensaje: "Estado del documento editado correctamante",
     });
+
   } catch (error) {
-    console.log(error);
     return res.status(500).json({
       mensaje: error,
     });
@@ -143,15 +151,17 @@ async function actualizarEstadoDocumento(req, res) {
 
 async function obtenerDocumentos(req, res) {
   try {
+
     const documentos = await Documento.find().populate({
-      path: "historial.idUsuarioFirmante",
-      populate: { path: "idPersona" },
+      path     : "historial.idUsuarioFirmante",
+      populate : { path: "idPersona" },
     });
+
     return res.status(200).json({
-      mensaje: documentos,
+      mensaje : documentos,
     });
+
   } catch (error) {
-    console.log(error);
     return res.status(500).json({
       mensaje: error,
     });
@@ -163,16 +173,17 @@ async function obtenerDocumentoPorNumero(req, res) {
     const documento = await Documento.findOne({ nroDocumento: req.params.nro });
     if (!documento) {
       return res.status(400).json({
-        statusCode: 400,
-        mensaje: `No se ha encontrado un documento con el número ${req.params.nro}. Revisa tus parámetros.`,
+        statusCode : 400,
+        mensaje    : `No se ha encontrado un documento con el número ${req.params.nro}. Revisa tus parámetros.`,
       });
     }
     return res.status(200).json({
-      mensaje: documento,
+      mensaje : documento,
     });
+
   } catch (error) {
     return res.status(500).json({
-      mensaje: error,
+      mensaje : error,
     });
   }
 }
